9a06057a855440b8a84b369c6acf3ac478c67d7b
dharmeshrao <yadavdydv@gmail.com>
2021-12-07 18:13:40 +0530

    update

--- src/components/Projects/Project.jsx
+++ src/components/Projects/Project.jsx
@@ -1,8 +1,9 @@
 import "./project.css";
 import ScrollAnimation from "react-animate-on-scroll";
 import mpl from "../../images/mpl.png";
-// import firstcry from "../../images/firstcry.png";
+import firstcry from "../../images/firstcry.png";
 import mmt from "../../images/mmt.png";
+import dev from "../../images/devfinder.png";
 const Project = ({ elem }) => {
   return (
     <div ref={elem} className="project">
@@ -92,38 +93,74 @@ const Project = ({ elem }) => {
           </div>
         </div>
       </ScrollAnimation>
+      <ScrollAnimation animateOnce={true} animateIn="fadeIn">
+        <div className="box">
+          <img className="projectimg" src={dev} alt="" />
+          <div className="details">
+            <div className="linkdiv">
+              <h3
+                onClick={() => {
+                  window.location.href =
+                    "https://github.com/dharmeshrao/devFinder";
+                }}
+                className="links"
+              >
+                GITHUB
+              </h3>
+              <h3
+                onClick={() => {
+                  window.location.href = "https://devfinder-dharmeshrao.vercel.app/";
+                }}
+                className="links"
+              >
+                DEMO
+              </h3>
+            </div>
+            <h3 className="heading">Devfinder</h3>
+            <div className="description">
+              <p>
+                A simple react app where you can search your Github account.
+                Solo project, checkout link is in the readme file.
+              </p>
+            </div>
+            <div className="btndiv">
+              <button>REACT</button>
+              <button>JAVASCRIPT</button>
+              <button>AXIOS</button>
+              <button>STYLED COMPONENTS</button>
+            </div>
+          </div>
+        </div>
+      </ScrollAnimation>
+      <ScrollAnimation animateOnce={true} animateIn="fadeIn">
+        <div className="box">
+          <div className="imgdiv">
+            <img className="projectimg" src={firstcry} alt="" />
+          </div>
+          <div className="details">
+            <div className="linkdiv">
+              <h3 onClick={()=>{
+                window.location.href = "https://github.com/dharmeshrao/Firstcry-Project"
+              }} className="links">GITHUB</h3>
+              <h3 className="links">DEMO</h3>
+            </div>
+            <h3 className="heading">Firstcry.com</h3>
+            <div className="description">
+              <p>
+                A web application where you can purchase clothing for infants
+              </p>
+            </div>
+            <div className="btndiv">
+              <button>MONGODB</button>
+              <button>JAVASCRIPT</button>
+              <button>EXPRESSJS</button>
+              <button>EJS</button>
+            </div>
+          </div>
+        </div>
+      </ScrollAnimation>
     </div>
   );
 };
 
 export default Project;
-
-
-
-
-
-// {/* <ScrollAnimation animateOnce={true} animateIn="fadeIn">
-// <div className="box">
-//   {/* <div className="imgdiv"> */}
-//   <img className="projectimg" src={firstcry} alt="" />
-//   {/* </div> */}
-//   <div className="details">
-//     <div className="linkdiv">
-//       <h3 className="links">GITHUB</h3>
-//       <h3 className="links">DEMO</h3>
-//     </div>
-//     <h3 className="heading">Firstcry.com</h3>
-//     <div className="description">
-//       <p>
-//         A web application where you can purchase clothing for infants
-//       </p>
-//     </div>
-//     <div className="btndiv">
-//       <button>MONGODB</button>
-//       <button>JAVASCRIPT</button>
-//       <button>EXPRESSJS</button>
-//       <button>EJS</button>
-//     </div>
-//   </div>
-// </div>
-// </ScrollAnimation> */}
